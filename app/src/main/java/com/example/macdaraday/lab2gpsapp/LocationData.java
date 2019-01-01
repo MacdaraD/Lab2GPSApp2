40ce7eebccc191b193d44f83e31820c304a93881
Macdara Day <macdaraday@gmail.com>
2019-01-01 18:31:42 +0000

    Hope this works

---
+++ app/src/main/java/com/example/macdaraday/lab2gpsapp/LocationData.java
@@ -0,0 +1,15 @@
+package com.example.macdaraday.lab2gpsapp;
+
+public class LocationData {
+
+    public double latitude;
+    public double longitude;
+
+
+    public LocationData(double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+}
+
